9ca154b47ea50cfa9a20cda26bc4ae972944a891
Jake Utley <jutley@whitepages.com>
2016-05-11 14:37:24 -0700

    Updated to have function write to its own region, and removed async functionality

--- src/main/scala/com/hiya/mobileprofile/MobileProfileUpdater.scala
+++ src/main/scala/com/hiya/mobileprofile/MobileProfileUpdater.scala
@@ -2,15 +2,15 @@ package com.hiya.mobileprofile
 
 import java.io.ByteArrayInputStream
 import java.nio.charset.StandardCharsets
-import java.util.concurrent.{TimeUnit}
 
-import com.amazonaws.regions.{Region, Regions}
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClient
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
 import com.amazonaws.services.dynamodbv2.model._
-
-import scala.collection.JavaConverters._
 import com.amazonaws.services.lambda.runtime.events.SNSEvent
 
+import scala.collection.JavaConverters._
+
+// TODO we may need to link up with Timur to share this as the serialization object
 case class MobileProfile(phoneNumber: String, profile: String)
 
 /**
@@ -20,9 +20,7 @@ class MobileProfileUpdater {
 
   //TODO: get from Typesafe config
   val table = "caller_ids"
-  val dynamoDB = new AmazonDynamoDBAsyncClient()
-  val regions = List(Regions.US_WEST_2, Regions.EU_CENTRAL_1, Regions.AP_SOUTHEAST_1, Regions.AP_SOUTHEAST_2, Regions.SA_EAST_1)
-  val timeout = 3
+  val dynamoDB = new AmazonDynamoDBClient()
 
   val scalaMapper = {
     import com.fasterxml.jackson.databind.ObjectMapper
@@ -35,30 +33,26 @@ class MobileProfileUpdater {
     result.map( profileAsJson => {
       deserializeMessageAndUpdate(profileAsJson)
     })
-
     val cutePrint = result.asJava
     println(cutePrint)
   }
 
-  def deserializeMessageAndUpdate(profileAsJson: String): List[UpdateItemResult] = {
+  def deserializeMessageAndUpdate(profileAsJson: String): UpdateItemResult = {
     val stream = new ByteArrayInputStream(profileAsJson.getBytes(StandardCharsets.UTF_8));
     val profile = scalaMapper.readValue(stream, classOf[MobileProfile])
     println("Got profile " + profile)
-    regions.map {
-      updateRegionsWithProfile(profile)
-    }
+    updateRegionWithProfile(profile)
   }
 
-  def updateRegionsWithProfile(profile: MobileProfile): (Regions) => UpdateItemResult = {
-    r =>
-      val region = Region.getRegion(r)
-      println("Updating Region " + region)
-      dynamoDB.setRegion(region)
-      val update = new AttributeValueUpdate().withAction(AttributeAction.PUT).withValue(new AttributeValue().withS(profile.profile))
-      val request: UpdateItemRequest = createUpdateRequest(profile, update)
-      println("Sending request " + request)
-      //TODO: exception handling on failure
-      dynamoDB.updateItemAsync(request).get(timeout, TimeUnit.MINUTES)
+  def updateRegionWithProfile(profile: MobileProfile): UpdateItemResult = {
+    val region = Regions.getCurrentRegion
+    println("Updating Region " + region)
+    dynamoDB.setRegion(region)
+    val update = new AttributeValueUpdate().withAction(AttributeAction.PUT).withValue(new AttributeValue().withS(profile.profile))
+    val request: UpdateItemRequest = createUpdateRequest(profile, update)
+    println("Sending request " + request)
+    //TODO: exception handling on failure
+    dynamoDB.updateItem(request)
   }
 
   def createUpdateRequest(profile: MobileProfile, update: AttributeValueUpdate): UpdateItemRequest = {
