7d45e3b177c0d81734e18d512e9bebcebeb32610
Jake Utley <jutley@whitepages.com>
2016-05-12 10:20:08 -0700

    added full test coverage for CloudWatchMonitoring.

--- src/main/scala/com/hiya/mobileprofile/CloudWatchMonitoring.scala
+++ src/main/scala/com/hiya/mobileprofile/CloudWatchMonitoring.scala
@@ -7,9 +7,13 @@ import com.typesafe.config.Config
 import scala.collection.JavaConverters._
 
 class CloudWatchMonitoring(cloudWatchClient: AmazonCloudWatch, config: Config) extends Monitoring {
+  require(cloudWatchClient != null, "should not get null client")
+  require(config != null, "should not get null config")
+
   val namespace = config.getString("namespace")
 
   override def writeMetric(metric: String, value: Double): Unit = {
+    require(metric != null && metric.trim.nonEmpty, "should not get null or empty")
     val dataPoint = new MetricDatum()
       .withValue(value)
       .withMetricName(metric)
--- src/test/scala/com/hiya/mobileprofile/CloudWatchMonitoringTest.scala
+++ src/test/scala/com/hiya/mobileprofile/CloudWatchMonitoringTest.scala
@@ -29,11 +29,59 @@ class CloudWatchMonitoringTest extends FunSpec with ShouldMatchers with MockFact
         .withMetricName(metric)
       val expectedRequest = new PutMetricDataRequest()
         .withNamespace(expectedNamespace)
-        .withMetricData((Seq[MetricDatum](dataPoint).asJava))
+        .withMetricData(Seq[MetricDatum](dataPoint).asJava)
       (client.putMetricData _).expects(expectedRequest).once()
           // When: I write a metric
       monitor.writeMetric(metric, value)
       // Then: My metric is written in my namespace with metricdata in client
+    }
+
+    it("rejects null client") {
+      //Given: a config
+      val config = mock[Config]
+      // Expect: I create with null and an IllegalArgumentException is thrown
+      an [IllegalArgumentException] should be thrownBy new CloudWatchMonitoring(null, config)
+    }
+
+    it("rejects null config") {
+      //Given: a cloudwatch client
+      val client = mock[AmazonCloudWatch]
+      // Expect: I create with null and an IllegalArgumentException is thrown
+      an [IllegalArgumentException] should be thrownBy new CloudWatchMonitoring(client, null)
+    }
+
+    it("rejects null metric") {
+      // Given: A configuration
+      val expectedNamespace = "testo"
+      val config = mock[Config]
+      (config.getString _).expects("namespace").returns(expectedNamespace).once()
+      // And: A client to cloudwatch
+      val client = mock[AmazonCloudWatch]
+      // And a CloudwatchMonitoring
+      val monitor : Monitoring = new CloudWatchMonitoring(client, config)
+      // And: a invalid metric to log
+      val metric = null
+      // And: A value for that metric
+      val value = Double.NaN
+      // Expect: I write a metric and an IllegalArgumentException is thrown
+      an [IllegalArgumentException] should be thrownBy monitor.writeMetric(metric, value)
+    }
+
+    it("rejects empty metric") {
+      // Given: A configuration
+      val expectedNamespace = "testo"
+      val config = mock[Config]
+      (config.getString _).expects("namespace").returns(expectedNamespace).once()
+      // And: A client to cloudwatch
+      val client = mock[AmazonCloudWatch]
+      // And a CloudwatchMonitoring
+      val monitor : Monitoring = new CloudWatchMonitoring(client, config)
+      // And: a invalid metric to log
+      val metric = "   "
+      // And: A value for that metric
+      val value = Double.NaN
+      // Expect: I write a metric and an IllegalArgumentException is thrown
+      an [IllegalArgumentException] should be thrownBy monitor.writeMetric(metric, value)
     }
   }
 }
