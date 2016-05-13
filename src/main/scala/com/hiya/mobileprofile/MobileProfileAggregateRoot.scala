9173474eb28f93b4d5549a0bdbe5dbaa6c01995b
V <nvaidyanathan@whitepages.com>
2016-05-12 18:35:10 -0700

    decoupled Aggregate Root from Lambda.

---
+++ src/main/scala/com/hiya/mobileprofile/MobileProfileAggregateRoot.scala
@@ -0,0 +1,41 @@
+package com.hiya.mobileprofile
+
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.cloudwatch.{AmazonCloudWatch, AmazonCloudWatchClient}
+import com.amazonaws.services.dynamodbv2.{AmazonDynamoDB, AmazonDynamoDBClient}
+import com.typesafe.config.ConfigFactory
+
+import scala.util.control.NonFatal
+
+/**
+  * The AGGREGATE ROOT/top-level FACADE for Mobile Profile updates.
+  * http://martinfowler.com/bliki/DDD_Aggregate.html
+  * http://blog.sapiensworks.com/post/2012/04/18/DDD-Aggregates-And-Aggregates-Root-Explained.aspx
+  */
+object MobileProfileAggregateRoot extends Logging {
+  private lazy val config = ConfigFactory.load("mobile-profile-update.conf")
+  private lazy val cloudWatchClient : AmazonCloudWatch = new AmazonCloudWatchClient()
+  private lazy val monitor: Monitoring = new CloudWatchMonitoring(cloudWatchClient, config)
+
+  private lazy val recoveryStrategy = PartialFunction[Throwable, Unit] {
+    case NonFatal(t) =>
+      monitor.writeMetric("WriteFailure", 1.0)
+      throw t
+  }
+
+  private lazy val region = Regions.getCurrentRegion
+  private lazy val dynamoDB : AmazonDynamoDB = new AmazonDynamoDBClient().withRegion(region)
+
+  private lazy val repository: MobileProfileRepository =
+    new RecoverableMobileProfileRepository(
+      new DynamoDbMobileProfileRepository(dynamoDB, config.getConfig("mobile-profile-update.table")), recoveryStrategy)
+
+  private lazy val deserializer: Deserializer[String, MobileProfile] = new MobileProfileDeserializer
+
+  def writeMessages(messages: Seq[String]): Unit = {
+    messages.foreach { profileAsJson =>
+      repository.save(deserializer.deserialize(profileAsJson))
+    }
+    writeLog(messages:_*)
+  }
+}
--- src/main/scala/com/hiya/mobileprofile/MobileProfileUpdater.scala
+++ src/main/scala/com/hiya/mobileprofile/MobileProfileUpdater.scala
@@ -13,36 +13,9 @@ import scala.util.control.NonFatal
 /**
   * Inspired from https://aws.amazon.com/blogs/compute/writing-aws-lambda-functions-in-scala/
   */
-class MobileProfileUpdater extends Logging {
-  import MobileProfileUpdater._
-
+class MobileProfileUpdater {
   def update(event: SNSEvent): Unit = {
     val messages: Seq[String] = event.getRecords.asScala.toSeq.map(record => record.getSNS.getMessage)
-    messages.foreach { profileAsJson =>
-      repository.save(deserializer.deserialize(profileAsJson))
-    }
-    val cutePrint = messages
-    writeLog(cutePrint:_*)
+    MobileProfileAggregateRoot.writeMessages(messages)
   }
-}
-
-object MobileProfileUpdater {
-  lazy val config = ConfigFactory.load("mobile-profile-update.conf")
-  lazy val cloudWatchClient : AmazonCloudWatch = new AmazonCloudWatchClient()
-  lazy val monitor: Monitoring = new CloudWatchMonitoring(cloudWatchClient, config)
-
-  lazy val recoveryStrategy = PartialFunction[Throwable, Unit] {
-    case NonFatal(t) =>
-      monitor.writeMetric("WriteFailure", 1.0)
-      throw t
-  }
-
-  lazy val region = Regions.getCurrentRegion
-  lazy val dynamoDB : AmazonDynamoDB = new AmazonDynamoDBClient().withRegion(region)
-
-  lazy val repository: MobileProfileRepository =
-    new RecoverableMobileProfileRepository(
-      new DynamoDbMobileProfileRepository(dynamoDB, config.getConfig("mobile-profile-update.table")), recoveryStrategy)
-
-  lazy val deserializer: Deserializer[String, MobileProfile] = new MobileProfileDeserializer
 }
